339d7bdac935fb7a9740b65b7f1c746b73bdde6f
Philip Castiglione <philipcastiglione@gmail.com>
2018-03-31 10:30:48 +0800

    counter increment can be called by anyone for now

--- app/controllers/counter_controller.rb
+++ app/controllers/counter_controller.rb
@@ -1,16 +1,22 @@
 # Actions relating to the counter singleton.
 class CounterController < ApplicationController
   before_action :get_counter
+  skip_before_action :verify_authenticity_token, only: [:increment]
 
   def index; end
 
   def increment
-    @counter.increment
+    @counter.increment if me?
   end
 
   private
 
   def get_counter
     @counter = Counter.singleton
+  end
+
+  def me?
+    # TODO: check auth header/token
+    true
   end
 end
