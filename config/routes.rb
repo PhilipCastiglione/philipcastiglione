13336ca69dce80ecb95eb4605d238d0676079fd4
Philip Castiglione <philipcastiglione@gmail.com>
2018-03-31 16:21:41 +0800

    enabled redis, actioncable

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
+
   get 'counter', to: 'counter#index'
   post 'counter', to: 'counter#increment'
 
