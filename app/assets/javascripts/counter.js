70b0c4c65ae26e731553d62386d2366b6f9432c2
Philip Castiglione <philipcastiglione@gmail.com>
2018-04-01 11:54:37 +0800

    added javascript for counter page sound recording

--- app/assets/javascripts/counter.js
+++ app/assets/javascripts/counter.js
@@ -1,2 +1,42 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).on("turbolinks:load", () => {
+  var microm = new Microm();
+  var mpeg = null;
+  var play = document.getElementById('play');
+  var pause = document.getElementById('pause');
+  var isRecording = false;
+
+  var toggleRecording = function() {
+    if (isRecording) {
+      play.classList.remove('hidden');
+      pause.classList.add('hidden');
+    } else {
+      play.classList.add('hidden');
+      pause.classList.remove('hidden');
+    }
+    isRecording = !isRecording;
+  }
+
+  var startAudioCapture = function(e) {
+    if (!isRecording) {
+      toggleRecording();
+      microm.record()
+        .catch(() => { alert('error recording') });
+    }
+  };
+
+  var endAudioCapture = function(e) {
+    if (isRecording) {
+      toggleRecording();
+      microm.stop().then(function(result) {
+        // mpeg = result;
+
+        microm.play();
+      });
+    }
+  };
+
+  play.addEventListener("click", startAudioCapture, true);
+  pause.addEventListener("click", endAudioCapture, true);
+});
