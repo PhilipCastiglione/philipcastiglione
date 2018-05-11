e5d084438a8484612ce7403471c9361f727e0696
Philip Castiglione <philipcastiglione@gmail.com>
2018-05-11 10:08:30 +1000

    disabling counter for now

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
 
-  get 'counter', to: 'counter#index'
-  post 'counter', to: 'counter#increment'
+  #get 'counter', to: 'counter#index'
+  #post 'counter', to: 'counter#increment'
 
   root 'static#index'
 end
